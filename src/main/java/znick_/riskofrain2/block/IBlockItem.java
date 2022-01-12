d6983bf840c2c29be6b5b99b8bc71250212499f4
NickIap <NickIap@users.noreply.github.com>
2022-01-12 00:17:21 -0500

    Lower commit size

--- src/main/java/znick_/riskofrain2/block/IBlockItem.java
+++
@@ -1,9 +0,0 @@
-package znick_.riskofrain2.block;
-
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.item.ItemBlock;
-
-public interface IBlockItem {
-	public abstract Class<? extends ItemBlock> getItemBlockClass();
-}
