3c6b2166641f043bd65842d83145e34c37de47aa
NickIap <NickIap@users.noreply.github.com>
2022-01-12 00:15:47 -0500

    Lower commit size

--- src/main/java/znick_/riskofrain2/item/ror/proc/item/OnHealItem.java
+++
@@ -1,9 +0,0 @@
-package znick_.riskofrain2.item.ror.proc.item;
-
-import net.minecraftforge.event.entity.living.LivingHealEvent;
-import znick_.riskofrain2.api.mc.PlayerData;
-
-public interface OnHealItem {
-
-	public abstract void procOnHeal(LivingHealEvent event, PlayerData player, int itemCount);
-}
--- src/main/java/znick_/riskofrain2/util/file/RiskOfRain2Files.java
+++
@@ -1,8 +0,0 @@
-package znick_.riskofrain2.util.file;
-
-import znick_.riskofrain2.RiskOfRain2;
-
-public class RiskOfRain2Files {
-
-	public static final String BUFFS = RiskOfRain2.MODID + ":textures/gui/buffs/";
-}
